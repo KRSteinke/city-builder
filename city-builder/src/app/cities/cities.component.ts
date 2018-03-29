44c4e914e00ae1631a2b844b0abaa39d973ca45c
Kurt Steinke <Kurt.Steinke@nisc.coop>
2018-03-29 17:35:40 -0500

    CB-1 Added base cities component and cities class. Updated UI with placeholder city

---
+++ city-builder/src/app/cities/cities.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {City} from "../hero";
+
+@Component({
+  selector: 'app-cities',
+  templateUrl: './cities.component.html',
+  styleUrls: ['./cities.component.css']
+})
+export class CitiesComponent implements OnInit {
+
+  city: City = {
+    uuid: 1,
+    name: 'Gardania',
+    populationCount: 1500
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
